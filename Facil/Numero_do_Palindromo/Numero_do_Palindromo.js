33db03e822336ecf3326064ed777ac7018f62c3f
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-02-24 11:20:04 -0300

    feat: resolve problema Número do Palíndromo

---
+++ Facil/Numero_do_Palindromo/Numero_do_Palindromo.js
@@ -0,0 +1,12 @@
+
+
+let valorEntrada = 121;
+
+let convertidoString = valorEntrada.toString();
+
+
+let stringInvertida = convertidoString.split('').reverse().join('');
+let resposta;
+
+
+console.log(convertidoString  === stringInvertida);
